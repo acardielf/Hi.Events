d635d2a22d4fc8a4998ceed487b89a1f19e63c63
Ángel Cardiel Ferrero <angel.cardiel@protonmail.com>
2025-02-26 23:42:31 +0100

    fix: floatval

--- backend/app/DomainObjects/AccountConfigurationDomainObject.php
+++ backend/app/DomainObjects/AccountConfigurationDomainObject.php
@@ -6,11 +6,11 @@ class AccountConfigurationDomainObject extends Generated\AccountConfigurationDom
 {
     public function getFixedApplicationFee(): float
     {
-        return $this->getApplicationFees()['fixed'] ?? config('app.default_application_fee_fixed');
+        return floatval($this->getApplicationFees()['fixed']) ?? config('app.default_application_fee_fixed');
     }
 
     public function getPercentageApplicationFee(): float
     {
-        return $this->getApplicationFees()['percentage'] ?? config('app.default_application_fee_percentage');
+        return floatval($this->getApplicationFees()['percentage']) ?? config('app.default_application_fee_percentage');
     }
 }
